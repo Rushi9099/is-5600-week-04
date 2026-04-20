135a31aad1eada571b0be9313e6c24882b46e157
Rushi9099 <rushichandra.5@gmail.com>
2026-04-20 00:43:36 +0000

    Week_5 Updated

--- app.js
+++ app.js
@@ -12,16 +12,26 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.use(middleware.cors)
 app.use(bodyParser.json())
 
+// ROOT
 app.get('/', api.handleRoot)
+
+// PRODUCTS
 app.get('/products', api.listProducts)
 app.get('/products/:id', api.getProduct)
 app.post('/products', api.createProduct)
-app.put('/products/:id', api.updateProduct)
+app.put('/products/:id', api.editProduct)
 app.delete('/products/:id', api.deleteProduct)
 
+// ORDERS
+app.get('/orders', api.listOrders)
+app.post('/orders', api.createOrder)
+app.put('/orders/:id', api.editOrder)
+app.delete('/orders/:id', api.deleteOrder)
+
+// ERROR HANDLING
 app.use(middleware.notFound)
 app.use(middleware.handleError)
 
 app.listen(port, () => {
-  console.log("Server listening on http://localhost:" + port)
+  console.log(`Server running at http://localhost:${port}`)
 })
